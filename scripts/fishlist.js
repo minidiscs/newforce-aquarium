13282dadbd6b5496fc6d9a5f16050bffd8235e4a
minidiscs <shanedbutler@gmail.com>
2022-06-23 08:42:41 -0700

    tips section added to fishes

--- scripts/fishlist.js
+++ scripts/fishlist.js
@@ -31,7 +31,11 @@ export const fishList = () => {
         </article>
 `
     }
-    fishHTML += `</section><section class="tips"><aside class="tips__tip">${tips.salinity}</aside></section></div>`
+    fishHTML += `</section><section class="tips"><aside class="tips__tip"><div class="tip__header"><h2 class="tips__name">Martin's Fish Tips</h2></div><p class="tip__details">Black clown goby, tilefish catshark sea grape starfish, banana wrasse starfish wobbegong shark, grouper batfish ear snail are at the bottom. Milk shark swim Asian clam soldierfish. Batfish bonnethead tilefish Banan.
+
+    Wrasse papershell coral hogfish. Seahorse rock lobster, papershell shark or scorpionfish is, faucet snail king crab weasel shark. Wabash pigtoe ear snail starfish, at hawkfish pebblesnail batfish crab. Coral hogfish snake eel goatfish and Black clown goby fanatic jumping at a nurse shark. Mystery snail lionfis.
+    
+    Papershell houndshark. Manta ray with lionfish ect. Hippo tang gold damsel peaclam, swam at Mandarinfish, puffer fish is sea grape. Hippo tang gold damsel peaclam.<p></aside></section></div> `
 
     return fishHTML
 }
