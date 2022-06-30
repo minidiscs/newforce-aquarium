9e598c666b49686ca8d2c7a70dda1a81dca36ebf
minidiscs <shanedbutler@gmail.com>
2022-06-30 15:31:37 -0400

    add primary keys

--- scripts/database.js
+++ scripts/database.js
@@ -7,6 +7,7 @@
 const database = {
     fish: [
         {
+            id: 1,
             species: "Sardine",
             image: "assets/sardine.png",
             nickname: "Pilchardo",
@@ -16,6 +17,7 @@ const database = {
             tip: "tip: clean tank every 5 weeks, keep salinity at 42% & temp at 67°F"
         },
         {
+            id: 2,
             species: "Salt Trout",
             image: "assets/salt-trout.png",
             nickname: "Roberto",
@@ -25,6 +27,7 @@ const database = {
             tip: "tip: clean tank every 3 weeks, keep salinity at 32% & temp at 72°F"
         },
         {
+            id: 3,
             species: "Peruvian Anchoveta",
             image: "assets/peruvian-anchoveta.png",
             nickname: "Enrique",
@@ -34,6 +37,7 @@ const database = {
             tip: "tip: clean tank every 3 weeks, keep salinity at 32% & temp at 72°F"
         },
         {
+            id: 4,
             species: "Silver Hake",
             image: "assets/silver-hake.png",
             nickname: "Katarina",
@@ -43,6 +47,7 @@ const database = {
             tip: "tip: clean tank every 6 weeks, keep salinity at 64% & temp at 59°F"
         },
         {
+            id: 5,
             species: "Chinook Salmon",
             image: "assets/chinook_salmon.png",
             nickname: "Josephine",
@@ -52,6 +57,7 @@ const database = {
             tip: "tip: clean tank every 5 weeks, keep salinity at 42% & temp at 67°F"
         },
         {
+            id: 6,
             species: "Atlantic Mackerel",
             image: "assets/atlantic-mackerel.png",
             nickname: "Mercurio",
@@ -61,6 +67,7 @@ const database = {
             tip: "tip: clean tank every 6 weeks, keep salinity at 64% & temp at 59°F"
         },
         {
+            id: 7,
             species: "Spanish Mackerel",
             image: "assets/spanish-mackerel.png",
             nickname: "Alexis",
@@ -70,6 +77,7 @@ const database = {
             tip: "tip: clean tank every 5 weeks, keep salinity at 42% & temp at 67°F"
         },
         {
+            id: 8,
             species: "Mahi Mahi",
             image: "assets/mahi-mahi.png",
             nickname: "Torpedo",
@@ -79,6 +87,7 @@ const database = {
             tip: "tip: clean tank every 3 weeks, keep salinity at 64% & temp at 67°F"
         },
         {
+            id: 9,
             species: "Monkfish",
             image: "assets/monkfish.png",
             nickname: "Diablo",
@@ -90,36 +99,42 @@ const database = {
     ],
     locations: [
         {
+            id: 1,
             location: "Delaware River",
             image: "assets/delaware-river.webp",
             locale: "Trenton, New Jersey",
             tip: "Make sure you pack plenty of sunscreen and bug spray. The locals are abundantly friendly, but the mosquitos most certainly are not!"    
         },
         {
+            id: 2,
             location: "Salton Sea",
             image: "assets/salton-sea.webp",
             locale: "Mortmar, California",
             tip: "There's still life in this old man-made lake. However, it's like a ghost town though if you are looking for human contact. Bring a dog companion to raise morale."
         },
         {
+            id: 3,
             location: "Frenchman Bay",
             image: "assets/frenchman-bay.webp",
             locale: "Bar Harbor, Maine",
             tip: "You will get Lyme's Disease if you don't cover up and spray repellant. Or you can just stay in the water, the ticks can't get you there!"
         },
         {
+            id: 4,
             location: "Lago de La Niña",
             image: "assets/lago-de-la-nina.png",
             locale: "Piura, Peru",
             tip: "Located in the capital of Peru's northwestern region, Lago de La Niña is the definition of 'que tranquilla'. Be sure to visit Isla Lobos de Tierra for some rare terrestrial species"
         },
         {
+            id: 5,
             location: "Coos Bay",
             image: "assets/coos-bay.png",
             locale: "North Bend, Oregon",
             tip: "Historically known as Marshfield, Coos Bay celebrates a history in shipbuilding and lumber products. Also there's fish!"
         },
         {
+            id: 6,
             location: "Mobile Bay",
             image: "assets/mobile-bay.jpg",
             locale: "Mobile, Alabama",
